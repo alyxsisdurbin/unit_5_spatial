2fd3605e867dc63c19747fb1b809cbf54e7f4fb2
alyxsisdurbin <hdurbin@email.sc.edu>
2023-03-30 11:01:04 -0400

    notes about bathy

--- class notes/class_notes_2023-03-28.R
+++ class notes/class_notes_2023-03-28.R
@@ -16,6 +16,7 @@ head(chl_df)
 
 # See range of data to set good limits for color palette
 hist(chl_df$chl_a)
+
 hist(log10(chl_df$chl_a)) # now we can see more of the variability in the data
 
 cols = rainbow(7, rev=TRUE)[-1] # reverse rainbow color hex codes, drops the first color (purple);
@@ -74,4 +75,102 @@ bath_m_raw = marmap::getNOAA.bathy(lon1 = lon_bounds[1],
                                    lat2 = lat_bounds[2], 
                                    resolution = 4) # resolution default: 4 arcminutes
 
+bath_m_df = marmap::fortify.bathy(bath_m_raw) # positive values are land, negative values are sea
+bath_m = bath_m_df %>%
+  mutate(depth_m = ifelse(z>20, NA, z)) %>% # 20m gives us wiggle room from sea level for tides/coastline
+  dplyr::select(-z)
 
+head(bath_m)
+head(world_map)
+
+# plot raster data
+# plot raster data
+GOM_bath_map = ggplot()+
+  geom_raster(data = bath_m , aes(x = x, y = y, fill = depth_m)) + 
+  geom_polygon(data = world_map, aes(x = long, y = lat, group = group), fill = "darkgrey", color = "black") + # add coastline; group keeps multipolygon coordinates separated into distinct groups # color=NA
+  coord_fixed(1.3, xlim = lon_bounds, ylim = lat_bounds, expand=FALSE) + # Crop map edges
+  scale_fill_gradientn(colors=c("black", "darkblue", "lightblue"), 
+                       values = scales::rescale(c(-6000, -300, 0)), # rescale to make 2 different gradients (rescale function from scales package in tidyverse)
+                       name="Depth (m)") +
+  ylab("Lat") + xlab("Lon") + theme_bw() 
+
+GOM_bath_map # print to screen
+
+# plot contour lines
+GOM_bath_map_contours = ggplot() +
+  geom_contour(data=bath_m, aes(x=x, y=y, z=depth_m), 
+               breaks=c(-100), linewidth=c(0.25), color="grey") +
+  geom_contour(data=bath_m, aes(x=x, y=y, z=depth_m), 
+               breaks=c(-200), linewidth=c(0.5), color="grey") +
+  geom_contour(data=bath_m, aes(x=x, y=y, z=depth_m), 
+               breaks=c(-500), linewidth=c(0.75), color="grey") +
+  geom_polygon(data = world_map, aes(x = long, y = lat, group = group), fill = "darkgrey", color = "black") + 
+  coord_fixed(1.3, xlim = lon_bounds, ylim = lat_bounds, expand=FALSE) 
+  
+ggsave(GOM_bath_map_contours, filename='figures/GOM_bath_contours.pdf', device="pdf", height=5, width=7)
+
+#rasterize the bathy object
+
+bath_m_raster = marmap::as.raster(bath_m_raw)
+chl_GOM_raster
+bath_m_raster
+# Note the CRS is the same for both rasters WGS84
+# Extent is slightly different 
+# bath_m resolution is higher than chl resolution
+
+names(bath_m_raster) = "bath_m"
+names(chl_GOM_raster)="chl_a"
+
+# resample bath_m to match chl_a
+bath_layer_chl_dims = raster::resample(bath_m_raster, chl_GOM_raster)
+
+# now that extent, origin, resolution and projection match, create raster stack
+raster_stack = stack(chl_GOM_raster, bath_layer_chl_dims)
+raster_stack
+plot(raster_stack)
+
+# convert to data frame
+stack_df = data.frame(raster::rasterToPoints(raster_stack))
+head(stack_df)
+summary(stack_df)
+
+
+
+
+#O'Reilly
+# chl_a benchmarks for oligo- meso- and eutrophic ocean waters derived from SeaWiFS data
+oligo_chl_a = 0.1 # chl_a < 0.1 mg/m^3
+eutro_chl_a = 1.67 # chl_a > 1.67 mg/m^3
+
+stack_df = stack_df %>%
+  mutate(trophic_index = case_when(chl_a < oligo_chl_a ~ "oligotrophic",
+                                   chl_a > oligo_chl_a & chl_a < eutro_chl_a ~ "mesotrophic",
+                                   chl_a > eutro_chl_a ~ "eutrophic"))  %>%
+  mutate(trophic_index = as.factor(trophic_index))
+
+# What portion of our area of interest is classified as oligotrophic, mesotrophic and eutrophic?
+table(stack_df$trophic_index)  # no oligotrophic waters in GOM region
+## 
+
+trophic_status = stack_df %>% 
+  filter(!is.na(trophic_index)) %>%
+  group_by(trophic_index) %>% 
+  summarize(n=n()) %>%
+  mutate(proportion = n/sum(n))
+
+# Plot histogram of bathymetric depth (m) for each trophic index
+ggplot() +
+  geom_histogram(aes(x=bath_m), data=stack_df) + # %>% filter(!is.na(trophic_index))) +
+  facet_wrap(~trophic_index)
+
+ggplot() +
+  geom_boxplot(aes(y=bath_m, x=trophic_index), data=stack_df)
+
+stack_df %>% filter(trophic_index=="eutrophic", bath_m > 50) 
+# plot trophic raster data
+trophic_map = ggplot()+
+  geom_raster(data = stack_df, aes(x = x, y = y, fill = trophic_index)) + 
+  geom_polygon(data = world_map, aes(x = long, y = lat, group = group)) + # add coastline; group keeps multipolygon coordinates separated into distinct groups
+  coord_fixed(1.3, xlim = lon_bounds, ylim = lat_bounds, expand=FALSE) + # Crop map edges
+  ylab("Lat") + xlab("Lon") + theme_bw() 
+trophic_map
