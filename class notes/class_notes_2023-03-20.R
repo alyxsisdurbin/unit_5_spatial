e262361c263a8ad894c4b50ab3c94c7ebd2e43c2
alyxsisdurbin <hdurbin@email.sc.edu>
2023-03-30 11:17:34 -0400

    notes for today

---
+++ class notes/class_notes_2023-03-20.R
@@ -0,0 +1,27 @@
+# HAD
+# 2023-03-30
+
+library(sf) # simple features (spatial vector data) st_read, st_transform
+# library(broom) # part of tidyverse
+library(tidyverse)
+library(mapdata)  # map_data
+library(marmap) # getNOAA.bathy()
+
+### Whales and Polygon
+
+USA_crit_hab = st_read(dsn = "data/North_Atlantic_Right_Whale_Critical_Habitat/",
+                       layer = "North_Atlantic_Right_Whale_Critical_Habitat")
+USA_crit_hab_sf = st_transform(USA_crit_hab, crs=4326) #this is the WGS 84 projection,
+
+CAN_crit_hab = read.csv("data/NARW_canadian_critical_habitat_2017.csv")
+head(CAN_crit_hab)
+
+CAN_crit_hab_sf = CAN_crit_hab %>%
+  st_as_sf(coords=c("lon", "lat"), crs=4326)
+
+
+
+
+
+# Carcass location data
+carcass = read.csv('data/RW_carcasses_2017.csv')
